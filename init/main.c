42b570e9893fb9be518c5f961f2642ad21ba9073
Adam Skladowski <a39.skl@gmail.com>
2023-06-22 22:09:52 +0200

    early

--- init/main.c
+++ init/main.c
@@ -556,6 +556,8 @@ static void __init mm_init(void)
 	pti_init();
 }
 
+void __init ramoops_earlycon_setup(u32 phys, u32 size);
+
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
@@ -609,7 +611,7 @@ asmlinkage __visible void __init start_kernel(void)
 	sort_main_extable();
 	trap_init();
 	mm_init();
-
+      ramoops_earlycon_setup(0xef000000, 0xbf800);
 	ftrace_init();
 
 	/* trace_printk can be enabled here */
