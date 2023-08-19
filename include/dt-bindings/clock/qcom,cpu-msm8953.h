47bd5b65a145dc304f7068ea71b82c1023c48e49
Adam Skladowski <a39.skl@gmail.com>
2023-08-19 21:28:26 +0200

    cpu-clk:Add MSM8953 header

---
+++ include/dt-bindings/clock/qcom,cpu-msm8953.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_CPU_MSM8953_H
+#define _DT_BINDINGS_CLK_QCOM_CPU_MSM8953_H
+
+#define APCS_MUX_C1_CLK		0
+#define APCS_MUX_CCI_CLK	1
+#define APCS_CPU_PLL0		2
+#define APCS_MUX_C0_CLK		3
+
+#endif
